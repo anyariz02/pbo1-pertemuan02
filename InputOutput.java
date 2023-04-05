7323cd295196194ebdc2e6425304424386786a6b
anyariz02 <anyanime.id@gmail.com>
2023-04-05 12:36:11 +0800

    Input Output Class

--- InputOutput.java
+++ InputOutput.java
@@ -5,7 +5,7 @@ public class InputOutput{
    Scanner scanner = new Scanner(System.in);
    String nama;
    System.out.print("Masukkan nama anda: ");
-   nama = scanner.nextline();
-   System.out.print("Hasi, "+nama);
+   nama = scanner.nextLine();
+   System.out.print("Hai, "+nama);
 }
 }
